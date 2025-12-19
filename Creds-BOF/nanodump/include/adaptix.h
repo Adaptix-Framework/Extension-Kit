f9d32a402dfa0a2b6006bcea4cf1d66259968dec
Mikhail Shashin <shashinma@icloud.com>
2025-12-19 06:44:05 +0300

    Implemented API for Targets

--- Creds-BOF/nanodump/include/adaptix.h
+++ Creds-BOF/nanodump/include/adaptix.h
@@ -4,3 +4,4 @@
 
 DECLSPEC_IMPORT void AxAddScreenshot(char* note, char* data, int len);
 DECLSPEC_IMPORT void AxDownloadMemory(char* filename, char* data, int len);
+DECLSPEC_IMPORT void AxAddTarget(char* computer, char* domain, char* address, int os, char* os_desk, char* tag, char* info, int alive);
